e2342bcd303ef35bf0723e85f8c5c12a9be6589a
Fredrik Jönsson <fjo@kth.se>
2018-05-25 21:33:12 +0200

    Switch to quartz. Enable bunyan logging.

--- src/main/java/se/kth/integral/mecenat/MecenatRouter.java
+++ src/main/java/se/kth/integral/mecenat/MecenatRouter.java
@@ -4,16 +4,15 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 /**
- * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
- * <p/>
- * Use <tt>@Component</tt> to make Camel auto detect this route when starting.
+ * Camel route to run sql queries against ladok3 database on a schedule.
  */
 @Component
 public class MecenatRouter extends RouteBuilder {
     @Override
     public void configure() {
-        from("timer:hello?period={{timer.period}}").routeId("hello")
-        .to("sql:select * from UPPFOLJNING.IO_STUDENTUPPGIFTER where PERSONNUMMER = '197103210170'")
-        .to("log:out");
+        from("quartz://mecenat?cron={{ladok3.cron}}&trigger.timeZone={{quartz.timezone}}")
+            .routeId("se.kth.integral.mecenat")
+            .to("sql:select * from UPPFOLJNING.IO_STUDENTUPPGIFTER where PERSONNUMMER = '197103210170'")
+            .to("log:out");
     }
 }
