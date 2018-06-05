85b4d321f94daadea4a688c64065b6df39ddb027
Fredrik Jönsson <fjo@kth.se>
2018-06-05 17:55:39 +0200

    Started working on antagningar.

---
+++ src/main/resources/sql/antagningar.sql
@@ -0,0 +1,50 @@
+--
+-- Hämtar studie perioder, plockad från Ladok3 wiki och parametriserad
+-- https://confluence.its.umu.se/confluence/display/LDSV/Generera+fil+till+Mecenat
+--
+-- Behöver headrar:
+-- 'termin' = '20172'
+-- 'startDatum' = '2017-08-28'
+-- 'slutDatum' = '2018-01-14'
+--
+
+select
+    s.personnummer
+    ,s.fornamn
+    ,s.careof
+    ,s.utdelningsadress
+    ,s.postnummer
+    ,s.postort
+    ,s.land
+    ,s.telefonnummer
+    ,s.epostadress
+    ,a.STUDENT_UID
+    ,a.enhet_kod
+    ,a.YTTERSTA_KURSPAKETERING_KOD
+    ,a.YTTERSTA_KURSPAKETERING_SV
+    ,a.YTTERSTA_KURSPAKETERINGSTYP_KOD
+    ,SUM(a.OMFATTNINGVARDE) AS OMFATTNING
+    ,MIN(a.UTBILDNINGSTILLFALLE_STARTDATUM)  AS STARTDATUM
+    ,MAX(a.UTBILDNINGSTILLFALLE_STARTDATUM)    AS SLUTDATUM
+from UPPFOLJNING.IO_STUDIEDELTAGANDE_ANTAGNING a inner join
+    UPPFOLJNING.IO_STUDENTUPPGIFTER s on s.STUDENT_UID = a.STUDENT_UID
+ where
+    a.UTBILDNINGSTILLFALLE_STARTDATUM >= :#${header.terminStartDatum} and
+    a.UTBILDNINGSTILLFALLE_STARTDATUM <= :#${header.terminSlutDatum} and
+    a.ENHET_KOD = 'HP'
+group by
+    s.personnummer
+    ,s.fornamn
+    ,s.careof
+    ,s.utdelningsadress
+    ,s.postnummer
+    ,s.postort
+    ,s.land
+    ,s.telefonnummer
+    ,s.epostadress
+    ,a.STUDENT_UID
+    ,a.enhet_kod
+    ,a.YTTERSTA_KURSPAKETERING_KOD
+    ,a.YTTERSTA_KURSPAKETERING_SV
+    ,a.YTTERSTA_KURSPAKETERINGSTYP_KOD
+order by s.personnummer desc
