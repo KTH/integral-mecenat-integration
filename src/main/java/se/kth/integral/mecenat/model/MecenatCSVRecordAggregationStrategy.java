c8930cd39bba7378a43b24d2a9f1f2d00c4c8933
Fredrik Jönsson <fjo@kth.se>
2018-06-09 15:49:50 +0200

    Add courses and research commitment.

--- src/main/java/se/kth/integral/mecenat/model/MecenatCSVRecordAggregationStrategy.java
+++ src/main/java/se/kth/integral/mecenat/model/MecenatCSVRecordAggregationStrategy.java
@@ -1,13 +1,16 @@
 package se.kth.integral.mecenat.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.InvalidPayloadException;
 import org.apache.camel.processor.aggregate.AggregationStrategy;
 
 public class MecenatCSVRecordAggregationStrategy implements AggregationStrategy {
-
     @Override
     public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
         if (oldExchange == null) {
@@ -24,11 +27,29 @@ public class MecenatCSVRecordAggregationStrategy implements AggregationStrategy 
 
         try {
             newRecords = newExchange.getIn().getMandatoryBody(ArrayList.class);
-            // TODO: Aggregera på smartare sätt för att undvika flera rader för samma person.
-            newRecords.addAll(oldRecords);
         } catch (InvalidPayloadException e) {
             return oldExchange;
         }
+
+        final Map<String, MecenatCSVRecord> uniqueRecords = new HashMap<String, MecenatCSVRecord>();
+
+        oldRecords.stream().map(r -> mergeRecords(uniqueRecords, r)).collect(Collectors.toList());
+        newRecords.stream().map(r -> mergeRecords(uniqueRecords, r)).collect(Collectors.toList());
+
+        newExchange.getIn().setBody(uniqueRecords.values());;
         return newExchange;
     }
+
+    private static MecenatCSVRecord mergeRecords(final Map<String, MecenatCSVRecord> records, final MecenatCSVRecord record) {
+        MecenatCSVRecord oldRecord = records.get(record.getPersonnummer());
+
+        if (oldRecord == null) {
+            records.put(record.getPersonnummer(), record);
+            return record;
+        }
+
+        oldRecord.setStudieomfattning(oldRecord.getStudieomfattning().add(record.getStudieomfattning()));
+        return oldRecord;
+    }
+
 }
