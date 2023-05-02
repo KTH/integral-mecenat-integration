1422d41384a47ea65facbaff8f42a86ba794a51b
Stefan Lachmann <slac@kth.se>
2023-05-02 14:12:42 +0200

    remove quotation marks

--- docker/opt/camel/run.sh
+++ docker/opt/camel/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "$MECENAT_INTEGRATION_CRT" | awk  '{gsub("\\\\n","\n")};1' | base64 -d > /opt/camel/etc/ladok3-user.crt && chmod 600 /opt/camel/etc/ladok3-user.crt
-echo "$MECENAT_INTEGRATION_KEY" | awk  '{gsub("\\\\n","\n")};1' | base64 -d > /opt/camel/etc/ladok3-user.key && chmod 600 /opt/camel/etc/ladok3-user.key
+echo $MECENAT_INTEGRATION_CRT | awk  '{gsub("\\\\n","\n")};1' | base64 -d > /opt/camel/etc/ladok3-user.crt && chmod 600 /opt/camel/etc/ladok3-user.crt
+echo $MECENAT_INTEGRATION_KEY | awk  '{gsub("\\\\n","\n")};1' | base64 -d > /opt/camel/etc/ladok3-user.key && chmod 600 /opt/camel/etc/ladok3-user.key
 
 stunnel /opt/camel/etc/stunnel.conf 2>&1 &
 
