7d68253ca2cde8864fb4efb597d743d4f1a876d7
Stefan Lachmann <83577786+stefanlachmann@users.noreply.github.com>
2023-05-02 13:57:56 +0200

    Revert "migrate: integral-mecenat-integration"

--- docker/opt/camel/run.sh
+++ docker/opt/camel/run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-echo "$MECENAT_INTEGRATION_CRT" | awk  '{gsub("\\\\n","\n")};1' | base64 -d > /opt/camel/etc/ladok3-user.crt && chmod 600 /opt/camel/etc/ladok3-user.crt
-echo "$MECENAT_INTEGRATION_KEY" | awk  '{gsub("\\\\n","\n")};1' | base64 -d > /opt/camel/etc/ladok3-user.key && chmod 600 /opt/camel/etc/ladok3-user.key
+echo $MECENAT_INTEGRATION_CRT | awk  '{gsub("\\\\n","\n")};1' | base64 -d > /opt/camel/etc/ladok3-user.crt && chmod 600 /opt/camel/etc/ladok3-user.crt
+echo $MECENAT_INTEGRATION_KEY | awk  '{gsub("\\\\n","\n")};1' | base64 -d > /opt/camel/etc/ladok3-user.key && chmod 600 /opt/camel/etc/ladok3-user.key
 
 stunnel /opt/camel/etc/stunnel.conf 2>&1 &
 
 if [ "$*" = "start" ]; then
-    exec /busybox httpd -f -v -p 80 -c /home/static/httpd.conf & java -jar application.jar
+    exec java -jar application.jar
 fi
 
-exec "$*"
+exec $*
