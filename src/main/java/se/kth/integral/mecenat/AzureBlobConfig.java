98a7e0f9b05dd546cd85bafb169678d53f8883c2
Joakim Granström <joakim.granstrom@irori.se>
2024-03-19 14:09:21 +0100

    add connections string as Value via app props

--- src/main/java/se/kth/integral/mecenat/AzureBlobConfig.java
+++ src/main/java/se/kth/integral/mecenat/AzureBlobConfig.java
@@ -4,13 +4,18 @@ import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @Configuration
 public class AzureBlobConfig {
 
+  @Value("${storage.account.connection.string}")
+  private String connectionString;
+
   @Bean
   public BlobServiceClient blobServiceClient() {
     // Replace with your connection string
-    return new BlobServiceClientBuilder().connectionString("{{storage.account.connection.string}}").buildClient();
+    return new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
   }
 }
